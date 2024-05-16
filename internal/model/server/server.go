ec748cd425bd896c4b179789f93124e017430431
jukuly <vpare070@uottawa.ca>
2024-05-16 13:41:55 -0400

    .

--- internal/model/server/server.go
+++ internal/model/server/server.go
@@ -15,7 +15,7 @@ func StartAdvertising(updated <-chan bool) {
 	adapter.Enable()
 
 	// Define the peripheral device info.
-	//adv := adapter.Advertisement()
+	adv := adapter.DefaultAdvertisement()
 	adv.Configure(bluetooth.AdvertisementOptions{
 		LocalName: "Go Bluetooth",
 	})
