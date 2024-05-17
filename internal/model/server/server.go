dab59af715e2502df014a7c7f8a25ed6d5bde4b7
jukuly <vpare070@uottawa.ca>
2024-05-17 11:45:36 -0400

    changed uuids

--- internal/model/server/server.go
+++ internal/model/server/server.go
@@ -14,11 +14,11 @@ func Pair() {
 func StartAdvertising(updated <-chan bool) {
 	adapter.Enable()
 
-	adapter.AddService(&bluetooth.Service{
-		UUID: [4]uint32{0x5F9B34FB, 0x80000080, 0x00001000, 0x1800},
+	service := bluetooth.Service{
+		UUID: [4]uint32{0xA07498CA, 0xAD5B474E, 0x940D16F1, 0xFBE7E8CD},
 		Characteristics: []bluetooth.CharacteristicConfig{
 			{
-				UUID:  [4]uint32{0x5F9B34FB, 0x80000080, 0x00001000, 0x2A00},
+				UUID:  [4]uint32{0x51FF12BB, 0x3ED846E5, 0xB4F9D64E, 0x2FEC021B},
 				Value: []byte("Hello World!"),
 				Flags: bluetooth.CharacteristicReadPermission,
 				WriteEvent: func(client bluetooth.Connection, offset int, value []byte) {
@@ -26,11 +26,16 @@ func StartAdvertising(updated <-chan bool) {
 					fmt.Printf("\toffset: %v\n", offset)
 					fmt.Printf("\tvalue: %v\n", value)
 				}},
-		}})
+		}}
+
+	adapter.AddService(&service)
 
 	adv := adapter.DefaultAdvertisement()
 	adv.Configure(bluetooth.AdvertisementOptions{
-		LocalName: "BLE Test Go"})
+		LocalName: "BLE Test Go",
+		ServiceUUIDs: []bluetooth.UUID{
+			service.UUID,
+		}})
 
 	adv.Start()
 
