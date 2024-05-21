9b3338bdf13654a20c7bcbf5954ce82beded4229
jukuly <vpare070@uottawa.ca>
2024-05-21 13:22:33 -0400

    .

--- cmd/ssMachMo/main.go
+++ cmd/ssMachMo/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/jukuly/ss_mach_mo/internal/model"
 	"github.com/jukuly/ss_mach_mo/internal/model/server"
 	"github.com/jukuly/ss_mach_mo/internal/view"
@@ -13,15 +16,15 @@ func main() {
 	server.Init(sensors)
 
 	go server.StartAdvertising()
-	/*go func() {
-		// delay for 15 seconds
-		for i := 0; i < 15; i++ {
-			view.Log(strconv.Itoa(15 - i))
+	go func() {
+		// delay for 25 seconds
+		for i := 0; i < 25; i++ {
+			view.Log(strconv.Itoa(25 - i))
 			time.Sleep(1 * time.Second)
 		}
 
 		server.StartPairing()
-	}()*/
+	}()
 
 	view.Start(sensors)
 
